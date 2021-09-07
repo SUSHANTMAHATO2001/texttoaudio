7d8e112266d87eea90c42100bc97b9c43764d628
SUSHANT <70790978+SUSHANTMAHATO2001@users.noreply.github.com>
2021-09-07 08:41:24 +0545

    Create audio.py

---
+++ audio.py
@@ -0,0 +1,34 @@
+import time
+import os
+import sys
+
+
+try:
+     import gtts
+
+except ImportError:
+       os.system('python2 -m pip install gTTs')
+
+os.system('clear')
+os.system('apt install play-audio -y')
+from gtts import *
+
+class color:
+       g = '\x1b[;33m'
+       r = '\x1b[;32m'
+
+def susan(s):
+    for a in s + '\n':
+       sys.stdout.write(a)
+       sys.stdout.flush()
+       time.sleep(1./12)
+class text:
+       p = 'Program By Sushant Mahato:2021-09-07th'
+       d = 'Devlop By Nepal'
+susan(color.g + text.p +'\n'+ color.r + text.d)
+
+t = raw_input('Inter The Letter:-')
+f = raw_input('Inter The Adio Name:-')
+
+a = gTTS(t)
+a.save(f+'.mp3')
